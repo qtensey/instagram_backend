df730d33514bb74eee4d4c743a11cada525b7c4a
tensey <tensey26@gmail.com>
2026-01-25 20:53:20 +0200

    main logic

---
+++ main.py
@@ -0,0 +1,8 @@
+from database import engine, Base
+from models import User, Post
+
+# Ця команда створює всі таблиці, які знайшла в Base
+# Вона еквівалентна CREATE TABLE IF NOT EXISTS
+print("Creating tables...")
+Base.metadata.create_all(bind=engine)
+print("Tables created successfully!")
