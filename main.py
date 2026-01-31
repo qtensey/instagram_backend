cdf6e50705fff784d6bd625078b8f1a562ed57b8
tensey <tensey26@gmail.com>
2026-01-31 17:47:04 +0200

    ...

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from models import User, Post
 # Ця команда створює всі таблиці, які знайшла в Base
 # Вона еквівалентна CREATE TABLE IF NOT EXISTS
 print("Creating tables...")
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 print("Tables created successfully!")
 
 with SessionLocal() as session:
